f967b879aa708554c4de51347c823068c3f71852
UCLA Radio Web <radio.web@media.ucla.edu>
2018-02-20 11:34:36 -0800

    Elrond updated at Tue Feb 20 11:34:34 PST 2018

--- ELROND_RIVENDELL.sh
+++ ELROND_RIVENDELL.sh
@@ -1,3 +1,6 @@
+THEDATE=`date`
 mysql -u web -pwebwizards -h 128.97.73.90 --xml Rivendell -e 'SELECT TITLE,ARTIST,ALBUM,GROUP_NAME  FROM CART' > /home/uclaradio/Desktop/Elrond/rivendell.xml
-
+git add -A
+git commit -m "Elrond updated at $THEDATE"
+git push -u origin master
 
